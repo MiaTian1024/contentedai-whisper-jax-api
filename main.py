e3b4a9512a119ea4735550358f629514c85a5499
MiaTian1024 <sddxtm123@gmail.com>
2023-09-20 21:12:21 +1200

    add video_processor

--- main.py
+++ main.py
@@ -1,93 +1,80 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from whisper_jax import FlaxWhisperPipline
-from audio import Audio
-from audioURL import AudioURL
+from video_processor import VideoProcessor
+import os
+
 
 app = FastAPI()
+video_processor = VideoProcessor()
 
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-# Loading the Whisper model
-def load_model():
-    pipeline = FlaxWhisperPipline("openai/whisper-base")
-    return pipeline
-
-# Transcription function using Whisper Jax
-def transcription(audio_file):
-    model = load_model()
-    outputs = model(audio_file, task="transcribe", return_timestamps=True)
-    return outputs
-
-
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my api"}
-
-
-@app.get("/process/")
-def get_video():
-    return {"message": "get video"}
-
+    return {"message": "Welcome to my API"}
 
 @app.post("/process/")
 async def process_video(url: str):
+    # Process a video from a given URL
     if not url:
         raise HTTPException(status_code=400, detail="Invalid URL")
 
-    audio_url = AudioURL(url)
-    video_filename = audio_url.save_video('video.mp4')
-    audio_filename = audio_url.save_audio()
+    # Save the video and audio, perform transcription
+    video_filename = video_processor.save_video(url, 'video.mp4')
+    audio_filename = video_processor.save_audio(url)
 
     if not video_filename or not audio_filename:
         raise HTTPException(status_code=500, detail="An error occurred while downloading the video or audio")
 
-    transcript_result = transcription(audio_filename)
+    transcript_result = video_processor.transcription(audio_filename)
 
     response_data = {
         'video_url': video_filename,
         'transcript': transcript_result['text']
     }
 
-    audio_url.remove_temporary_files(video_filename)
-    audio_url.remove_temporary_files(audio_filename)
+    # Clean up temporary files
+    video_processor.remove_temporary_files(video_filename)
+    video_processor.remove_temporary_files(audio_filename)
 
     return response_data
 
 
 @app.post("/upload/")
 def upload(file: UploadFile = File(...)):
+    # Upload a file and process it
     try:
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
             f.write(contents)
-    except Exception:
-        return {"message": "There was an error uploading the file"}
+    except Exception as e:
+        return {"message": f"There was an error uploading the file: {str(e)}"}
     finally:
         file.file.close()
 
-    input = file.filename
-    audio_file = Audio(input)
-    output = audio_file.convert_to_mp3()
-    print(output)
-    transcript_result = transcription(output)
+    input_file = file.filename
+    output_file = video_processor.convert_to_mp3(input_file)
+    transcript_result = video_processor.transcription(output_file)
 
     response_data = {
-        'file_name': input,
+        'file_name': input_file,
         'transcript': transcript_result['text']
     }
 
-    audio_file.remove_temporary_files(input)
-    audio_file.remove_temporary_files(output)
+    # Clean up temporary files
+    video_processor.remove_temporary_files(input_file)
+    video_processor.remove_temporary_files(output_file)
     
     return response_data
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
